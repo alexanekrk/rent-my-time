07df616018c12399f11ea3611a985d42a3a84c4d
Alexane Kurek <alexane.kurek7@gmail.com>
2023-05-29 17:35:53 -0400

    create the model pingeon

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :pigeons, dependant: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
