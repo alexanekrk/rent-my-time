fec992cc08fd260560adc48b5d8ef1f862d13eab
Pablo Hennique <phennique@gmail.com>
2023-05-30 12:26:59 -0400

    updated seed to include users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,32 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Clean the database..."
+puts "Cleaning users and pigeons from the database..."
 Pigeon.destroy_all
 User.destroy_all
 
 puts "Adding 3 users... Tom, Alexane and Pablo"
 
-User.new
+pablo = User.create(
+  first_name: 'Pablo',
+  last_name: 'Hennique',
+  email: 'phennique@gmail.com',
+  password: '111111'
+)
+
+alexane = User.create(
+  first_name: 'Alexane',
+  last_name: 'France',
+  email: 'alexane@france.com',
+  password: '111111'
+)
+
+tom = User.create(
+  first_name: 'Tom',
+  last_name: 'Gorilla',
+  email: 'tom@grilla.com',
+  password: '111111'
+)
 
 puts "Adding some pigeons..."
 
@@ -22,7 +41,7 @@ puts "Adding some pigeons..."
     color: %w[grey black yellow blue pink red green orange].sample,
     age: rand(1..8),
     price: rand(10..100),
-    user_id: '1'
+    user_id: [pablo, alexane, tom].sample.id
   )
   puts "Create the pigeon id #{pigeon.id}"
 end
