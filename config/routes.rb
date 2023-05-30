84d19ac60542a7a7e8a01f28056f3ebb615b3d75
TomKim <ehdgus5289@gmail.com>
2023-05-30 17:03:20 -0400

    before merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-
   get 'pigeons/my_pigeons', to: 'pigeons#my_pigeons', as: :my_pigeons
   resources :pigeons do
-    resources :bookings
+    resources :bookings, only: [ :new, :create, :edit, :update ]
   end
+  resources :bookings, only: [ :index, :show, :delete]
 end
