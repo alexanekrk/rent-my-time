078e33f6b017a7d1f50fc19d58d28d574fbc869e
Alexane Kurek <alexane.kurek7@gmail.com>
2023-05-30 11:13:06 -0400

    add a ssed and button delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Clean the database..."
+Pigeon.destroy_all
+
+puts "Add some pigeons..."
+
+10.times do
+  pigeon = Pigeon.create(
+    name: Faker::Name.name,
+    color: %w[grey black yellow blue pink red green orange].sample,
+    age: rand(1..8),
+    price: rand(10..100),
+    user_id: '1'
+  )
+  puts "Create the pigeon id #{pigeon.id}"
+end
+
+puts 'All done'
