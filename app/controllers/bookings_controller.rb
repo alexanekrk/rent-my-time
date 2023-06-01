988cc50db7c5058870b265728866007bc576a944
TomKim <ehdgus5289@gmail.com>
2023-06-01 12:11:09 -0400

    working on rent_to

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,25 +3,29 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+  def show
+  end
+
   def new
     @booking = Booking.new
     @pigeon = Pigeon.find(params[:pigeon_id])
   end
 
   def create
-    year = params[:booking]['date(1i)']
-    month = params[:booking]['date(2i)']
-    day = params[:booking]['date(3i)']
-    date = "#{day}-#{month}-#{year}"
-    date = Date.strptime(date, '%d-%m-%Y')
+    start = params[:booking]['start_date']
+    end_d = params[:booking]['end_date']
     location = params[:booking][:location]
     pigeon = params[:pigeon_id]
-    @booking = Booking.new(date: date, location: location, user_id: current_user.id, pigeon_id: pigeon )
+    @booking = Booking.new(start_date: start, end_date: end_d , location: location, user_id: current_user.id, pigeon_id: pigeon )
     @booking.save
     redirect_to bookings_path
   end
 
-  def destroy
+  def rent_from
+    @bookings = Booking.all
+  end
 
+  def rent_to
+    @bookings = Booking.all
   end
 end
