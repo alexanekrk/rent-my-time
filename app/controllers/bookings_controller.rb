4705d4f7bf71181bec2ecb804ef8ef7fc5ecece8
TomKim <ehdgus5289@gmail.com>
2023-06-01 14:53:55 -0400

    reject done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,10 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:id])
+    @pigeon = Pigeon.find(@booking.pigeon_id)
+    @seller = User.find(@pigeon.user_id)
+    @buyer = User.find(@booking.user_id)
   end
 
   def new
@@ -19,6 +23,12 @@ class BookingsController < ApplicationController
     @booking = Booking.new(start_date: start, end_date: end_d , location: location, user_id: current_user.id, pigeon_id: pigeon )
     @booking.save
     redirect_to bookings_path
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.delete
+    redirect_to rent_to_path
   end
 
   def rent_from
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   get '/search', to: "pages#search"
 
   get 'pigeons/my_pigeons', to: 'pigeons#my_pigeons', as: :my_pigeons
+
   get "bookings/booking_rent_from", to: 'bookings#rent_from', as: :rent_from
   get "bookings/booking_rent_to", to: 'bookings#rent_to', as: :rent_to
+
   resources :pigeons do
     resources :bookings, only: [ :new, :create, :edit, :update ]
   end
-  resources :bookings, only: [ :index, :show, :delete]
 
-
+  resources :bookings, only: [ :index, :show, :destroy]
 end
